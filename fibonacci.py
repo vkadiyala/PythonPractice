1918c59b5e6dd7f19c8eb8b55088f63598f0cf63
vkadiyala <ec2-user@ip-172-31-27-199.us-east-2.compute.internal>
2020-02-13 20:02:03 +0000

    updated the file

--- fibonacci.py
+++ fibonacci.py
@@ -1,5 +1,16 @@
+#1 1 2 3 5 8
 
+print("want to see the fibonacci series for a number? give  try! type a number below")
+num = int(input())
 
+v1 = v2 = 1
+fib = []
+fib.append(1)
 
-
-
+while v2 < num:
+    fib.append(v2)
+    total = v1 + v2
+    v1 = v2
+    v2 = total
+    
+print(f"Here is the possible fibonacci sequence for your number: {fib}")
